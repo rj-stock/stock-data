e5a0a3ab0c7c6c7ee22801b0c3034618ca212afe
rj-stock <rongji_h@hotmail.com>
2023-04-16 21:47:57 +0800

    添加同花顺移动端的 last360 数据抓取

--- crawler/impl/ths/crawl_last360.ts
+++ crawler/impl/ths/crawl_last360.ts
@@ -1,5 +1,5 @@
 /**
- * 从爱问财打开页面 http://www.iwencai.com/unifiedwap/result?w=600000 收集的数据请求地址。
+ * 从移动端打开页面 https://m.10jqka.com.cn/stockpage/hs_600000 收集的数据请求地址。
  */
 import { parseJsonp } from "../../../deps.ts"
 import { Crawler } from "../../crawler.ts"
@@ -9,21 +9,21 @@ import { LastXResponsJson, parseLastXJson2StockData, period2LineUrlPath, thsRequ
 /** 同花顺股票数据器爬取实现 */
 const crawl: Crawler = async (code: string, period = KPeriod.Day, debug = false): Promise<StockData> => {
   const sp = period2LineUrlPath(period)
-  const url = `http://d.10jqka.com.cn/v6/line/17_${code}/${sp}/last3600.js?ts=${Date.now()}`
+  const url = `http://d.10jqka.com.cn/v6/line/hs_${code}/${sp}/last360.js?ts=${Date.now()}`
   const response = await fetch(url, thsRequestInit)
   if (!response.ok) throw new Error(`从同花顺获取 "${code}" 数据失败：${response.status} ${response.statusText}`)
 
   /**
-   * 响应体数据结构例子: quotebridge_v6_line_17_000158_00_last3600({
+   * 响应体数据结构例子: quotebridge_v6_line_hs_000158_00_last360({
    *   "data":"yyyyMMdd,open,high,low,close,vol,amo,...;...",
    *   "name":"\u5e38\u5c71\u5317\u660e","start":"20000724","total":"5361",
    *   "marketType":"HS_stock_sz","issuePrice":"","today":"20230412",
-   *   "rt":"0930-1130,1300-1500","num":3600,
+   *   "rt":"0930-1130,1300-1500","num":360,
    *   "year":{"2000":110,...,"2023":65},
    * })
    */
   const txt = await response.text()
-  if (debug) Deno.writeTextFile(`temp/10jqka-v6-line-last3600-${code}-${period}.js`, txt)
+  if (debug) Deno.writeTextFile(`temp/10jqka-v6-line-last360-${code}-${period}.js`, txt)
 
   const j = parseJsonp(txt) as LastXResponsJson
   return parseLastXJson2StockData(code, j, period)
