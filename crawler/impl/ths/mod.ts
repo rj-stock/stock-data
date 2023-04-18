bbce8fa56b83876c32a102689f6b800eb046e23f
rj-stock <rongji_h@hotmail.com>
2023-04-19 00:10:33 +0800

    Polishing

--- crawler/impl/ths/mod.ts
+++ crawler/impl/ths/mod.ts
@@ -1,4 +1,10 @@
+/** 爬取所有 K 线数据，但没有成交金额 */
 export { default as crawlAllK } from "./crawl_all.ts"
+/** 爬取最后 3600 条 K 线数据，有成交金额 */
 export { default as crawlLast3600K } from "./crawl_last3600.ts"
+/** 爬取最后 360 条 K 线数据，有成交金额 */
 export { default as crawlLast360K } from "./crawl_last360.ts"
-export { default as crawlLastTime } from "./crawl_time.ts"
+/** 爬取最后交易日的所有分时数据 */
+export { default as crawlTodayAllTime } from "./crawl_time.ts"
+/** 爬取最后交易日的最后那条 K 线的数据 */
+export { default as crawlTodayLatestK } from "./crawl_today.ts"
