5a9453e39766cd1edd2abd95bb173568517a3210
rj-stock <rongji_h@hotmail.com>
2023-04-20 11:31:20 +0800

    优化 crawl_today_repeat 增加当日开盘信息

--- crawler/impl/ths/crawl_today_repeat.ts
+++ crawler/impl/ths/crawl_today_repeat.ts
@@ -1,4 +1,4 @@
-import { formatDateTime, parseArgs, pathExistsSync, red, TerminalProgress } from "../../../deps.ts"
+import { blue, formatDateTime, gray, green, parseArgs, pathExistsSync, red, TerminalProgress } from "../../../deps.ts"
 import { KPeriod, LatestKData } from "../../../types.ts"
 import { default as crawlTodayLatestK } from "./crawl_today.ts"
 import { default as crawlLast360K } from "./crawl_last360.ts"
@@ -7,12 +7,12 @@ import { default as crawlLast360K } from "./crawl_last360.ts"
 if (!pathExistsSync("temp")) Deno.mkdir("temp")
 
 // log arguments help
-console.info(`参数帮助：
+console.log(gray(`参数帮助：
   -c 指定代码，默认 600000
   -p 指定周期(day、week、month、year、minute1、minute5、minute30、minute60)，默认 day
   -s 指定间隔秒数，默认 1 秒
   -m 是否每次新数据输出到新行
-  如 "deno run -A ./crawler/impl/ths/crawl_today_repeat.ts -c FG9999 -p minute1 -s 5 -m"`)
+  如 "deno run -A ./crawler/impl/ths/crawl_today_repeat.ts -c FG9999 -p minute1 -s 5 -m"`))
 
 // parse cli arguments
 const args = parseArgs(Deno.args)
@@ -22,10 +22,19 @@ const seconds = Object.hasOwn(args, "s") ? parseInt(args["s"]) : 1
 const multiline = Object.hasOwn(args, "m")
 
 // 显示昨天数据
-const t = await crawlLast360K(code, KPeriod.Day)
-console.log(`${t.code} ${t.name}`)
-const preK = t.data[t.data.length - 2]
-console.log(`PreDayK=${JSON.stringify(preK)}`)
+const ks = await crawlLast360K(code, KPeriod.Day)
+console.info(ks.code + " " + ks.name)
+const preDayK = ks.data[ks.data.length - 2]
+const preDayK1 = ks.data[ks.data.length - 3]
+let zf = ((preDayK.c - preDayK1.c) / preDayK1.c * 100).toFixed(2) + "%"
+let k = `PreDayK=${JSON.stringify(Object.assign(preDayK, { zf: zf }))}`
+console.log(zf.startsWith("-") ? green(k) : red(k))
+
+// 显示当天开盘数据及期货昨日结算价
+const latestDayK = ks.data[ks.data.length - 1]
+zf = ((latestDayK.o - preDayK.c) / preDayK.c * 100).toFixed(2) + "%"
+k = `LatestDayK=${JSON.stringify({ t: latestDayK.t, o: latestDayK.o, p: latestDayK.p, ozf: zf })}`
+console.log(zf.startsWith("-") ? green(k) : red(k))
 
 // 初始化进度条
 const progress = new TerminalProgress({
@@ -55,7 +64,7 @@ async function repeat(code: string, seconds: number, period: KPeriod): Promise<v
     setInterval(async () => {
       try {
         const k = await crawl2File(code, period, false)
-        const zf = ((k.c - preK.c) / preK.c * 100).toFixed(2) + "%"
+        const zf = ((k.c - preDayK.c) / preDayK.c * 100).toFixed(2) + "%"
         progress.options.extra.up = !zf.startsWith("-")
         progress.options.extra.k = JSON.stringify({
           t: k.t,
--- deps.ts
+++ deps.ts
@@ -4,7 +4,7 @@ export { dirname, extname, join as joinPath } from "https://deno.land/std@0.180.
 export { extension } from "https://deno.land/std@0.180.0/media_types/mod.ts"
 export { format as formatDateTime } from "https://deno.land/std@0.180.0/datetime/mod.ts"
 export { parse as parseArgs } from "https://deno.land/std@0.180.0/flags/mod.ts"
-export { green, red } from "https://deno.land/std@0.180.0/fmt/colors.ts"
+export { blue, gray, green, red } from "https://deno.land/std@0.180.0/fmt/colors.ts"
 
 // 3-parts
 export { parse as parseJsonp } from "https://deno.land/x/nextrj_utils@0.11.0/jsonp.ts"
