243c3e032f17652111bf69740f274c7693fccd5e
rj-stock <rongji_h@hotmail.com>
2023-08-07 16:44:09 +0800

    实现从深交所网站爬取标的的K线数据-日、周、月、季度、年

---
+++ crawler/impl/sz/crawl_k_test.ts
@@ -0,0 +1,50 @@
+import { assert, assertEquals, assertObjectMatch } from "../../../deps.ts"
+import { KPeriod, StockKData } from "../../../types.ts"
+import { CrawlInit } from "../../crawler.ts"
+import crawl from "./crawl_k.ts"
+
+async function crawl2File(code: string, period = KPeriod.Day, init?: CrawlInit): Promise<StockKData> {
+  const stockK = await crawl(code, period, init)
+  if (init?.debug) await Deno.writeTextFile(`temp/sz-${code}-${period}.json`, JSON.stringify(stockK, null, 2))
+  return stockK
+}
+
+Deno.test("000001 day k all", async () => {
+  const list = await crawl2File("000001", KPeriod.Day, { debug: true })
+  assertEquals(list.code, "000001")
+  assertEquals(list.name, "平安银行")
+  assert(list.total >= 201, `expected >= 201 actual ${list.total}`)
+  assertEquals(list.dataCount, list.total)
+})
+
+Deno.test("000001 week k all", async () => {
+  const list = await crawl2File("000001", KPeriod.Week, { debug: true, start: 0 })
+  assertEquals(list.code, "000001")
+  assertEquals(list.name, "平安银行")
+  assert(list.total >= 201, `expected >= 201 actual ${list.total}`)
+  assertEquals(list.dataCount, list.total)
+})
+
+Deno.test("000001 month k all", async () => {
+  const list = await crawl2File("000001", KPeriod.Month, { debug: true, start: 0 })
+  assertEquals(list.code, "000001")
+  assertEquals(list.name, "平安银行")
+  assert(list.total >= 201, `expected >= 201 actual ${list.total}`)
+  assertEquals(list.dataCount, list.total)
+})
+
+Deno.test("000001 quarter k all", async () => {
+  const list = await crawl2File("000001", KPeriod.Quarter, { debug: true, start: 0 })
+  assertEquals(list.code, "000001")
+  assertEquals(list.name, "平安银行")
+  assert(list.total >= 130, `expected >= 130 actual ${list.total}`)
+  assertEquals(list.dataCount, list.total)
+})
+
+Deno.test("000001 year k all", async () => {
+  const list = await crawl2File("000001", KPeriod.Year, { debug: true, start: 0 })
+  assertEquals(list.code, "000001")
+  assertEquals(list.name, "平安银行")
+  assert(list.total >= 33, `expected >= 33 actual ${list.total}`)
+  assertEquals(list.dataCount, list.total)
+})
--- crawler/impl/sz/mod.ts
+++ crawler/impl/sz/mod.ts
@@ -1,2 +1,4 @@
-/** 爬取所有股票列表(仅包含标的代码和名称) */
+/** 爬取所有股票列表(仅包含标的代码和名称，因存在分页获取比较慢) */
 export { default as crawlList } from "./crawl_list.ts"
+/** 爬取标的的 K 线数据集(这个接口最多返回最近的 201 根 K 线) */
+export { default as crawlK } from "./crawl_k.ts"
